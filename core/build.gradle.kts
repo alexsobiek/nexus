f3da9e7e354042ddf25a878e28cb2349e8e05976
Alex Sobiek <alex@alexsobiek.com>
2022-06-12 01:03:43 -0500

    Added event bus priority test

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,4 +3,10 @@ plugins {
 }
 
 dependencies {
+    implementation("org.slf4j:slf4j-simple:1.7.36");
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1");
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
