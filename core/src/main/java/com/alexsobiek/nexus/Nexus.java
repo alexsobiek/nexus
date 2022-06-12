95519e3de7887a42ce599b5e338ec03c7954463d
Alex Sobiek <alex@alexsobiek.com>
2022-06-12 01:02:56 -0500

    Implemented builder

--- core/src/main/java/com/alexsobiek/nexus/Nexus.java
+++ core/src/main/java/com/alexsobiek/nexus/Nexus.java
@@ -1,4 +1,90 @@
 package com.alexsobiek.nexus;
 
+import com.alexsobiek.nexus.event.EventBus;
+import com.alexsobiek.nexus.thread.NexusThreadFactory;
+import com.alexsobiek.nexus.util.Lazy;
+import org.slf4j.Logger;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.Supplier;
+
 public class Nexus {
+    private final Logger logger;
+    private final NexusThreadFactory threadFactory;
+    private final ForkJoinPool commonPool;
+    private final Lazy<EventBus> eventBus;
+    private final Lazy<Scheduler> scheduler;
+
+    protected Nexus(int poolThreads, int schedulerThreads, NexusThreadFactory threadFactory, Logger logger) {
+        this.threadFactory = threadFactory;
+        this.logger = logger;
+        this.commonPool = new ForkJoinPool(poolThreads, threadFactory.getForkJoinFactory(), threadFactory.getThreadGroup(), true);
+        this.eventBus = new Lazy<>(() -> new EventBus(commonPool));
+        this.scheduler = new Lazy<>(() -> new Scheduler(schedulerThreads, threadFactory.getSimpleFactory()));
+    }
+
+    /**
+     * Runs the supplied task asynchronously
+     *
+     * @param task Task to run
+     */
+    public CompletableFuture<Void> async(Runnable task) {
+        return CompletableFuture.runAsync(task, commonPool);
+    }
+
+    /**
+     * Runs the task asynchronously, returns the value back as a future once complete
+     *
+     * @param supplier T Supplier
+     * @param <T>      Return type
+     * @return Future
+     */
+    public <T> CompletableFuture<T> supply(Supplier<T> supplier) {
+        return CompletableFuture.supplyAsync(supplier, commonPool);
+    }
+
+    /**
+     * Creates a new thread for the task to run on
+     *
+     * @param task Task to run
+     * @return Thread
+     */
+    public Thread thread(Runnable task) {
+        return threadFactory.getSimpleFactory().newThread(task);
+    }
+
+    /**
+     * Gets the common executor used by Nexus.
+     *
+     * @return Executor
+     */
+    public Executor executor() {
+        return commonPool;
+    }
+
+    /**
+     * Gets the event bus.
+     * EventBus object does not exist until the first time this method is called
+     *
+     * @return EventBus
+     */
+    public EventBus eventBus() {
+        return eventBus.get();
+    }
+
+    /**
+     * Gets the scheduler.
+     * Scheduler object does not exist until the first time this method is called
+     *
+     * @return EventBus
+     */
+    public Scheduler scheduler() {
+        return scheduler.get();
+    }
+
+    public static NexusBuilder builder() {
+        return new NexusBuilder();
+    }
 }
---
+++ core/src/main/java/com/alexsobiek/nexus/NexusBuilder.java
@@ -0,0 +1,94 @@
+package com.alexsobiek.nexus;
+
+import com.alexsobiek.nexus.thread.NexusThreadFactory;
+import com.alexsobiek.nexus.thread.impl.ImplNexusThreadFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NexusBuilder {
+    private int poolThreads = -1;
+    private int schedulerThreads = -1;
+    private NexusThreadFactory threadFactory;
+    private Logger logger;
+
+    /**
+     * Sets the amount of threads available for the common pool.
+     * If not set, all threads available will be used.
+     * Does not affect the amount of threads created with {@link Nexus#async(Runnable)}
+     *
+     * @param threads Threads for common pool
+     * @return NexusBuilder
+     */
+    public NexusBuilder threads(int threads) {
+        this.poolThreads = threads;
+        return this;
+    }
+
+
+    /**
+     * Sets the amount of threads to use for the {@link Scheduler}.
+     * If not, the same amount of threads set with {@link NexusBuilder#threads(int)} will be used.
+     * If {@link Nexus#scheduler()} is never called, no threads will be created.
+     *
+     * @param threads Number of threads to use
+     * @return NexusBuilder
+     */
+    public NexusBuilder schedulerThreads(int threads) {
+        this.schedulerThreads = threads;
+        return this;
+    }
+
+    /**
+     * Sets the thread factory to be used when creating threads
+     *
+     * @param factory Thread factory for creating threads
+     * @return NexusBuilder
+     */
+    public NexusBuilder threadFactory(NexusThreadFactory factory) {
+        this.threadFactory = factory;
+        return this;
+    }
+
+    /**
+     * Sets the SLF4J logger
+     *
+     * @param logger Logger to use
+     * @return NexusBuilder
+     */
+    public NexusBuilder logger(Logger logger) {
+        this.logger = logger;
+        return this;
+    }
+
+    // Methods use for building
+    private int pt() {
+        return poolThreads == -1
+                ? Runtime.getRuntime().availableProcessors()
+                : poolThreads;
+    }
+
+    private int st(int poolThreads) {
+        return schedulerThreads == -1
+                ? poolThreads
+                : schedulerThreads;
+    }
+
+    private NexusThreadFactory tf(Logger l) {
+        return threadFactory == null
+                ? new ImplNexusThreadFactory(l)
+                : threadFactory;
+    }
+
+    private Logger l() {
+        return logger == null
+                ? LoggerFactory.getLogger("Nexus")
+                : logger;
+    }
+
+    public Nexus build() {
+        Logger l = l();
+        int pt = pt();
+        int st = st(pt);
+        return new Nexus(pt, st, tf(l), l);
+    }
+}
