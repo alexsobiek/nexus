5bd3f8a98381bc040007e3502b9f7e8c9d71ae48
Alex Sobiek <alex@alexsobiek.com>
2022-06-20 19:34:07 -0500

    Implemented TCPSocket

--- tcp/src/main/java/com/alexsobiek/nexus/tcp/client/TCPClient.java
+++ tcp/src/main/java/com/alexsobiek/nexus/tcp/client/TCPClient.java
@@ -1,7 +1,15 @@
 package com.alexsobiek.nexus.tcp.client;
 
 import com.alexsobiek.nexus.tcp.TCPSocket;
+import com.alexsobiek.nexus.tcp.channel.Pipeline;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 
-public class TCPClient extends TCPSocket {
+import java.net.InetSocketAddress;
 
+public class TCPClient extends TCPSocket<Channel, Bootstrap> {
+
+    public TCPClient(InetSocketAddress address, int threads, Pipeline<?> pipeline) {
+        super(address, threads, pipeline);
+    }
 }
--- tcp/src/main/java/com/alexsobiek/nexus/tcp/server/TCPServer.java
+++ tcp/src/main/java/com/alexsobiek/nexus/tcp/server/TCPServer.java
@@ -2,13 +2,13 @@ package com.alexsobiek.nexus.tcp.server;
 
 import com.alexsobiek.nexus.tcp.TCPSocket;
 import com.alexsobiek.nexus.tcp.channel.Pipeline;
-import io.netty.channel.MultithreadEventLoopGroup;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ServerChannel;
 
 import java.net.InetSocketAddress;
 
-public class TCPServer extends TCPSocket {
-
-    public TCPServer(InetSocketAddress address, MultithreadEventLoopGroup nioGroup, Pipeline pipeline) {
-        super(address, nioGroup, pipeline);
+public class TCPServer extends TCPSocket<ServerChannel, ServerBootstrap> {
+    public TCPServer(InetSocketAddress address, int threads, Pipeline<?> pipeline) {
+        super(address, threads, pipeline);
     }
 }
